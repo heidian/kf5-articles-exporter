08f9294802631958b0da1a11f575a00bdeefc00f
web3nomad.eth <me@web3nomad.eth>
2023-12-10 02:26:33 +0800

    add section route

--- src/main.rs
+++ src/main.rs
@@ -69,6 +69,36 @@ async fn category_handler(Path(category_id): Path<String>) -> Html<String> {
     Html(html)
 }
 
+async fn section_handler(Path(section_id): Path<String>) -> Html<String> {
+    let section_id = section_id.parse::<i64>().unwrap();
+    let forums = load_json("forums").await;
+    let posts = load_json("posts").await;
+
+    let article_item_html = String::from(r#"<li><a href="/hc/kb/article/{{article_id}}/" title="{{article_title}}">{{article_title}}</a></li>"#);
+    let article_list_html = posts
+        .as_array().unwrap().iter()
+        .filter(|p| p["forum_id"] == section_id)
+        .map(|p| {
+            let article_item_html = article_item_html
+                .replace("{{article_id}}", &p["id"].to_string())
+                .replace("{{article_title}}", &p["title"].as_str().unwrap());
+            article_item_html
+        }).collect::<Vec<String>>().join("");
+
+    let section = forums
+        .as_array().unwrap().iter()
+        .find(|c| c["id"] == section_id).unwrap();
+    let section_html = load_html("snippets/section.html").await;
+    let section_html = section_html
+        .replace("{{section_title}}", section["title"].as_str().unwrap())
+        .replace("{{article_list}}", &article_list_html)
+        ;
+
+    let layout_html = load_html("layout.html").await;
+    let html = layout_html.replace("{{body}}", &section_html);
+    Html(html)
+}
+
 async fn article_handler(Path(article_id): Path<String>) -> Html<String> {
     let article_id = article_id.parse::<i64>().unwrap();
     let posts = load_json("posts").await;
@@ -78,6 +108,7 @@ async fn article_handler(Path(article_id): Path<String>) -> Html<String> {
         .find(|c| c["id"] == article_id).unwrap();
 
     let article_content = article["content"].as_str().unwrap().to_owned();
+    let article_content = article_content.replace("support.hey.shop", "support.heidianer.com");
     let re = Regex::new(r"https:\/\/files.kf5.com\/attachments\/download\/(\w+\/\w+\/\w+)\/").unwrap();
     let article_content = re.replace_all(
         &article_content,
@@ -106,6 +137,8 @@ async fn main() {
         .route("/hc", get(homepage_handler))
         .route("/hc/kb/category/:id", get(category_handler))
         .route("/hc/kb/category/:id/", get(category_handler))
+        .route("/hc/kb/section/:id", get(section_handler))
+        .route("/hc/kb/section/:id/", get(section_handler))
         .route("/hc/kb/article/:id", get(article_handler))
         .route("/hc/kb/article/:id/", get(article_handler))
         ;
